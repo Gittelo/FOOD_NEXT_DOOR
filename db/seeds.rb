e237b7294d54e739d17562ba490b778098d13d75
diogo telo <diogo_telo@hotmail.com>
2018-06-04 17:58:22 +0100

    Meals Created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts("Cleaning Database...")
+Account.destroy_all!
+Meal.destroy_all!
+Order.destroy_all!
+Review.destroy_all!
+User.destroy_all
+
+puts("Creating meals...")
+meal1 = Meals.create!(name: "Delicious Babon", description: "I didn't know Portugal shops closed early. The last two days, I got back post 9pm and there aren't many places open. Thankfully yelp came to the rescue with.", grams_per_dose: 500, ingredients: "Maionese, tomato and cheese")
+meal2 = Meals.create!(name: "Delicious Lasanha", description: "This is one of the most random reviews ever, but this is the best Chinese food I've ever had. We've been here twice and both times Mr. Lu was here and in the kitchen.", grams_per_dose: 400, ingredients: "Maionese")
+meal3 = Meals.create!(name: "Delicious Pasta", description: "what a surprise! I had no high expectation thinking it might be just another so-so Chinese restaurant like others in lisbon. most people came  here for the buffet", grams_per_dose: 300, ingredients: "tomato and cheese")
+meal4 = Meals.create!(name: "Great Sushi", description: "I've had Chinese food in 45 states , Toronto and British Columbia  Hands down ... NO JOKE the best DIM SUM and fresh Chinese dishes ever that have crossed my taste buds !!", grams_per_dose: 100, ingredients: "carrot")
+
