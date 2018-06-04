a0533d44b4f11fc451e1ff8bcc53b4210d9501e4
Antoine Quellier <antoinecquellier@gmail.com>
2018-06-04 17:59:50 +0100

    reviews seed + views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Review.create!(user: User.find(1), meal: Meal.find(1), rating: 3, description: 'That was good enough')
+Review.create!(user: User.find(2), meal: Meal.find(2), rating: 2, description: 'That was not good')
+Review.create!(user: User.find(3), meal: Meal.find(3), rating: 3, description: 'That was good')
+Review.create!(user: User.find(4), meal: Meal.find(4), rating: 4, description: 'That was really good')
+Review.create!(user: User.find(5), meal: Meal.find(4), rating: 4, description: 'That was really good')
