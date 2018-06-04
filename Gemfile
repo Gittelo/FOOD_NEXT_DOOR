ec97a39b27cd0aad627fdbd2a67f4db2a71d1c97
Antoine Quellier <antoinecquellier@gmail.com>
2018-06-04 17:24:10 +0100

    installed pundit

--- Gemfile
+++ Gemfile
@@ -31,3 +31,5 @@ group :development, :test do
 end
 
 gem 'money-rails'
+
+gem 'pundit'
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,45 @@
 class MealsController < ApplicationController
+  beforce_action :set_meals, only: [:show, :edit, :destroy]
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+    @meal= Order.new
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @meal.destroy
+    redirect_to meals_path
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :address, :price, :grams_per_dose, :ingredients)
+  end
+
+  def set_meals
+    @meal = Meal.find(params[:id])
+  end
+
 end
