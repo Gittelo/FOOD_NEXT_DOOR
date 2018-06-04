a7407c4e7bece2833d60cf5d536538e8349a3c23
diogo telo <diogo_telo@hotmail.com>
2018-06-04 23:01:18 +0100

    Pundit install and some css in the show page

--- app/policies/meal_policy.rb
+++ app/policies/meal_policy.rb
@@ -19,17 +19,17 @@ class MealPolicy < ApplicationPolicy
   # end
 
   def update?
-    true
+    user_is_cook?
   end
 
   def destroy?
-    true
+    user_is_cook?
   end
 
   private
 
   def user_is_cook?
-    user == record.user_is_cook
+    user == record.cook
   end
 
   class Scope < Scope
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,19 @@ Order.destroy_all
 Review.destroy_all
 User.destroy_all
 
-puts("Creating meals...")
-meal1 = Meal.create!(name: "Delicious Babon", description: "I didn't know Portugal shops closed early. The last two days, I got back post 9pm and there aren't many places open. Thankfully yelp came to the rescue with.", grams_per_dose: 500, ingredients: "Maionese, tomato and cheese")
-meal2 = Meal.create!(name: "Delicious Lasanha", description: "This is one of the most random reviews ever, but this is the best Chinese food I've ever had. We've been here twice and both times Mr. Lu was here and in the kitchen.", grams_per_dose: 400, ingredients: "Maionese")
-meal3 = Meal.create!(name: "Delicious Pasta", description: "what a surprise! I had no high expectation thinking it might be just another so-so Chinese restaurant like others in lisbon. most people came  here for the buffet", grams_per_dose: 300, ingredients: "tomato and cheese")
-meal4 = Meal.create!(name: "Great Sushi", description: "I've had Chinese food in 45 states , Toronto and British Columbia  Hands down ... NO JOKE the best DIM SUM and fresh Chinese dishes ever that have crossed my taste buds !!", grams_per_dose: 100, ingredients: "carrot")
-
 puts("Creating users...")
 user1 = User.create!(first_name: "Maria", last_name: "B",  address: "Lisbon",  email: "maria@fesf.fr",  password: "123456", cook_status: 0)
 user2 = User.create!(first_name: "Marta", last_name: "C",  address: "Cascais", email: "marta@fesf.fr",  password: "123456", cook_status: 0)
 user3 = User.create!(first_name: "Pedro", last_name: "F",  address: "Porto",   email: "pedro@fesf.fr",  password: "123456", cook_status: 0)
 user4 = User.create!(first_name: "Joana", last_name: "R",  address: "Faro",    email: "joana@fesf.fr",  password: "123456", cook_status: 0)
 user5 = User.create!(first_name: "Vasco", last_name: "C",  address: "Coimbra", email: "vasco@fesf.fr",  password: "123456", cook_status: 0)
+
+puts("Creating meals...")
+meal1 = Meal.create!(cook: user1,name: "Delicious Babon", description: "I didn't know Portugal shops closed early. The last two days, I got back post 9pm and there aren't many places open. Thankfully yelp came to the rescue with.", grams_per_dose: 500, ingredients: "Maionese, tomato and cheese")
+meal2 = Meal.create!(cook: user2,name: "Delicious Lasanha", description: "This is one of the most random reviews ever, but this is the best Chinese food I've ever had. We've been here twice and both times Mr. Lu was here and in the kitchen.", grams_per_dose: 400, ingredients: "Maionese")
+meal3 = Meal.create!(cook: user2,name: "Delicious Pasta", description: "what a surprise! I had no high expectation thinking it might be just another so-so Chinese restaurant like others in lisbon. most people came  here for the buffet", grams_per_dose: 300, ingredients: "tomato and cheese")
+meal4 = Meal.create!(cook: user3,name: "Great Sushi", description: "I've had Chinese food in 45 states , Toronto and British Columbia  Hands down ... NO JOKE the best DIM SUM and fresh Chinese dishes ever that have crossed my taste buds !!", grams_per_dose: 100, ingredients: "carrot")
+
 
 puts("Creating order...")
 order1 = Order.create!(user: User.find(1), meal: Meal.find(1), status: 0, doses: 2)
