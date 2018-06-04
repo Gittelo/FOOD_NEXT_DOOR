2d6c4eacb54c7047d51f4152fa52700141f1748e
Joana Reis <joanarfreis@gmail.com>
2018-06-04 18:26:31 +0100

    Seeds fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_153206) do
+ActiveRecord::Schema.define(version: 2018_06_04_154204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2018_06_04_153206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.integer "cook_id"
+    t.index ["cook_id"], name: "index_meals_on_cook_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -64,7 +66,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_153206) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "address"
-    t.integer "cook_status"
+    t.integer "cook_status", default: 0, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
