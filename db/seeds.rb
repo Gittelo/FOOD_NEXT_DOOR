03668b0558daa491490629dda846ebb917ae7cc2
Joana Reis <joanarfreis@gmail.com>
2018-06-04 18:05:41 +0100

    Added order seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+order1 = Order.create!(user: User.find(1), meal: Meal.find(1), status: 0, doses: 2)
+order2 = Order.create!(user: User.find(2), meal: Meal.find(2), status: 1, doses: 4)
+order3 = Order.create!(user: User.find(3), meal: Meal.find(3), status: 2, doses: 1)
+order4 = Order.create!(user: User.find(4), meal: Meal.find(4), status: 3, doses: 5)
+order5 = Order.create!(user: User.find(5), meal: Meal.find(3), status: 2, doses: 3)
+
+
