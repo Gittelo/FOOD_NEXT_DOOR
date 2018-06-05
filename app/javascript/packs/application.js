f666fc445e74a08fb69f5edc66a95b165af7fac5
diogo telo <diogo_telo@hotmail.com>
2018-06-05 17:37:26 +0100

    My_repository done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,13 @@
 import "bootstrap";
 
 import initMap from '../components/map.js';
-//const mapElement = document.getElementById('map');
-//initMap(mapElement);
+const mapElement = document.getElementById('map');
+initMap(mapElement);
 
-//import { autocomplete } from '../components/autocomplete';
-//autocomplete();
+import { autocomplete } from '../components/autocomplete';
+autocomplete();
+
+//import drag_box from '../components/drag_box'
+//drag_box();
 
 console.log("Hello from app/javascript/packs/application.js!");
--- app/javascript/packs/map.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap";
-
-import initMap from '../components/map.js'
-
-const mapElement = document.getElementById('map');
-initMap(mapElement);
-
-console.log("Hello from app/javascript/packs/map.js!");
