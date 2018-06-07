0da104382e2934afc99e4111d6d5469c7a09272e
diogo telo <diogo_telo@hotmail.com>
2018-06-07 19:13:10 +0100

    curly braces removed

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -58,7 +58,7 @@ const styles = [
 
 const initMap = function(mapElement) {
   if (mapElement) { // don't try to build a map if there's no div#map to inject in
-    const map = new GMaps({ el: '#map', lat: 0, lng: 0 } );..
+    const map = new GMaps({ el: '#map', lat: 0, lng: 0 } );
     const markers = JSON.parse(mapElement.dataset.markers);
     map.addMarkers(markers);
     if (markers.length === 0) {
