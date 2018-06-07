a08aac9e88e75d160d804f9bff15286ab6c9abfa
diogo telo <diogo_telo@hotmail.com>
2018-06-07 19:54:38 +0100

    just to save everything as a comment

---
+++ app/javascript/components/hide_show.js
@@ -0,0 +1,31 @@
+
+/*
+const hideElement = (element) => {
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+const changeCSSRule = (element, rule, value) => {
+  element.setAttribute('style', `${rule}: ${value};`);
+}
+const switchClasses = (element, class1, class2) => {
+  if (element.classList.contains(class1))
+  {
+    element.classList.remove(class1);
+    element.classList.add(class2);
+  } else if (element.classList.contains(class2))
+  {
+    element.classList.remove(class2);
+    element.classList.add(class1);
+  }
+}
+
+const map = document.getElementById('map');
+const button = document.getElementById('show-map');
+const cardsContainer = document.querySelector('.cards-container');
+const sectionLeft = document.getElementById('cards-section-left');
+const buttonReview = document.getElementById('show-reviews-btn');
+const reviewContainer = document.querySelector('.meal-review');
+*/
