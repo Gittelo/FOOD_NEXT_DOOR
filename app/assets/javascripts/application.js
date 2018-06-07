292e5b1f5e17d973d65cd599cde7e9c540440505
catarinacota <catarina.afc@gmail.com>
2018-06-07 18:02:29 +0100

    still doing review dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,13 +23,18 @@ const switchClasses = (element, class1, class2) => {
   }
 }
 
-
 const map = document.getElementById('map');
 const button = document.getElementById('show-map');
 const cardsContainer = document.querySelector('.cards-container');
 const sectionLeft = document.getElementById('cards-section-left');
+const buttonReview = document.getElementById('show-reviews-btn');
+const reviewContainer = document.querySelector('.meal-review');
 
 button.addEventListener('click', (event) => {
   hideElement(map);
   switchClasses(sectionLeft, 'col-md-8', 'col-md-12');
 });
+
+buttonReview.addEventListener('click', (event) => {
+  hideElement(reviewContainer);
+})
