106040d03e1ea563b9963ad40c25ad86d40db6a4
catarinacota <catarina.afc@gmail.com>
2018-06-07 17:42:44 +0100

    show and hide map width problem fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,35 @@
 //= require rails-ujs
 //= require_tree .
+
+const hideElement = (element) => {
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
+const changeCSSRule = (element, rule, value) => {
+  element.setAttribute('style', `${rule}: ${value};`);
+}
+const switchClasses = (element, class1, class2) => {
+  if (element.classList.contains(class1))
+  {
+    element.classList.remove(class1);
+    element.classList.add(class2);
+  } else if (element.classList.contains(class2))
+  {
+    element.classList.remove(class2);
+    element.classList.add(class1);
+  }
+}
+
+
+const map = document.getElementById('map');
+const button = document.getElementById('show-map');
+const cardsContainer = document.querySelector('.cards-container');
+const sectionLeft = document.getElementById('cards-section-left');
+
+button.addEventListener('click', (event) => {
+  hideElement(map);
+  switchClasses(sectionLeft, 'col-md-8', 'col-md-12');
+});
