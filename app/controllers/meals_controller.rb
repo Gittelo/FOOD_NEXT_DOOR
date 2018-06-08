c42d5897574f74dc7cae1b16b74b39218b5801b2
Joana Reis <joanarfreis@gmail.com>
2018-06-08 15:51:35 +0100

    search bar on landing page

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -19,10 +19,16 @@ class MealsController < ApplicationController
     end
 
     if params[:query].present?
-      @meals = Meal.search_by_name(params[:query])
-    else
-      @meals = Meal.all
+         @meals = Meal.search_by_address(params[:query])
+      else
+        @meals = Meal.all
     end
+
+    # if params[:query].present?
+    #   @meals = Meal.search_by_name(params[:query])
+    # else
+    #   @meals = Meal.all
+    # end
 
   end
 
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -7,6 +7,13 @@ class Meal < ApplicationRecord
       tsearch: { prefix: true }
     }
 
+    pg_search_scope :search_by_address,
+    against: [ :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
