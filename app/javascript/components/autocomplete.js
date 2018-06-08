5c77a6b2358a781fe67b85752f3778984028c897
Joana Reis <joanarfreis@gmail.com>
2018-06-08 16:58:37 +0100

    autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,6 +1,6 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    const mealsAddress = document.querySelector('input');
+    const mealsAddress = document.getElementById('search');
 
     if (mealsAddress) {
       var autocomplete = new google.maps.places.Autocomplete(mealsAddress, { types: [ 'geocode' ] });
