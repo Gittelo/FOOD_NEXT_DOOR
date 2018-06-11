e112c223d5d79d47ed3ff237d65f0d643a8d8cbd
diogo telo <diogo_telo@hotmail.com>
2018-06-11 12:16:52 +0100

    Before make a magration of Week_days

--- app/controllers/week_days_controller.rb
+++ app/controllers/week_days_controller.rb
@@ -1,2 +1,24 @@
 class WeekDaysController < ApplicationController
+
+
+  def new
+    @week_day = WeekDay.new
+    authorize @week_day
+  end
+
+  def create
+    @week_day = WeekDay.new(week_day_params)
+    raise
+  end
+
+  private
+
+  def week_day_params
+    params[:week_day]["date(1i)"] = 3000
+
+    params.require(:week_day).permit(:date, :last_order_time, :pick_up_time_start, :pick_up_time_end, :doses)
+  end
+
+
+
 end
