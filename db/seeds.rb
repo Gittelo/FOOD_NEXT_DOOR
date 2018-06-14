265222e83097ba9d4c158c22ae40313c21434ff3
Antoine Quellier <antoinecquellier@gmail.com>
2018-06-14 14:53:03 +0100

    seed correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,9 +56,9 @@ userJ4 = User.create!(first_name: "João",      last_name: "G",          address
 userJ5 = User.create!(first_name: "Manuel",    last_name: "G",          address: "Rua Tomás Ribero 15, Lisboa",            email: "manuel.g@fesf.fr",   password: "123456", cook_status: 0, photo_fake: 'https://wit.edu/sites/default/files/delgado-manuel.jpg')
 
 #change pictures
-userA1 = User.create!(first_name: "Typhany",   last_name: "G",          address: "Avenida do Brasil 23, Lisboa",           email: "typhany@fesf.fr",    password: "123456", cook_status: 0, photo_fake: 'https://wit.edu/sites/default/files/delgado-manuel.jpg')
-userA2 = User.create!(first_name: "Elise",     last_name: "G",          address: "Rua Seara Nova 1, Lisboa",               email: "elise@fesf.fr",      password: "123456", cook_status: 0, photo_fake: 'https://wit.edu/sites/default/files/delgado-manuel.jpg')
-userA3 = User.create!(first_name: "Lea",       last_name: "G",          address: "Avenida de Roma 1, Lisboa",              email: "lea@fesf.fr",        password: "123456", cook_status: 0, photo_fake: 'https://wit.edu/sites/default/files/delgado-manuel.jpg')
+userA1 = User.create!(first_name: "Typhany",   last_name: "G",          address: "Avenida do Brasil 23, Lisboa",           email: "typhany@fesf.fr",    password: "123456", cook_status: 0, photo_fake: 'https://i.pinimg.com/originals/2f/9d/4f/2f9d4f05948c5fe1c2aa06aebb69088c.jpg')
+userA2 = User.create!(first_name: "Elise",     last_name: "G",          address: "Rua Seara Nova 1, Lisboa",               email: "elise@fesf.fr",      password: "123456", cook_status: 0, photo_fake: 'http://www.lovely-hairstyles.com/wp-content/uploads/2016/11/Hair-Style-for-Black-Women.jpg')
+userA3 = User.create!(first_name: "Lea",       last_name: "G",          address: "Avenida de Roma 1, Lisboa",              email: "lea@fesf.fr",        password: "123456", cook_status: 0, photo_fake: 'https://www.womeninbiznetwork.com/wp-content/uploads/2017/02/2-500x500.png')
 userA4 = User.create!(first_name: "Tim",       last_name: "G",          address: "Rua Telhal 1, Lisboa",                   email: "tim@fesf.fr",        password: "123456", cook_status: 0, photo_fake: 'https://wit.edu/sites/default/files/delgado-manuel.jpg')
 
 
@@ -111,7 +111,7 @@ mealJ1 = Meal.create!(cook: userJ1,
                                     Our version of Quiche Lorraine is just that: made with a creamy, cheesy, savory center and baked on a flaky, warm crust for a brunch recipe that’s filling',
                       grams_per_dose: 300,
                       ingredients: 'Bacon, gruyère, eggs, pastry, olive oil, eggs, milk, cream',
-                      price: 7.5,
+                      price: 4.5,
                       address: userJ1.address,
                       photo: 'https://img.taste.com.au/yzM49jFu/taste/2017/02/classic-quiche-lorraine-121391-2.jpg')
 mealJ2 = Meal.create!(cook: userJ2,
@@ -129,7 +129,7 @@ mealJ3 = Meal.create!(cook: userJ3,
                                     The dish is a common meal on many tables throughout the country, though many claim the dish hails from the Beiras, a strip of land between the South of the Douro River and the North of the Tagus River.',
                       grams_per_dose: 700,
                       ingredients: 'Octupus, potatoes, onions, coriander, garlic, olive oil, bay leaves, salt and pepper.',
-                      price: 4.5,
+                      price: 8.5,
                       address: userJ3.address,
                       photo: 'https://labohemie.pt/wp-content/uploads/2017/12/Fama-dAlfama-polvo-%C3%A0-lagareiro.jpg')
 mealJ4 = Meal.create!(cook: userJ4,
@@ -180,7 +180,7 @@ mealA4 = Meal.create!(cook: userA4,
                       description: "This exotic revisited dish fits in perfectly with the French South West cuisine. This octopus stew takes plenty of time to prepare, since it requires long and slow simmering. The end result is a very tender octopus in a stew which will delight the most demanding taste buds. The chorizo and grass sublimate the fine and tasteful flesh of this mysterious animal. ",
                       grams_per_dose: 800,
                       ingredients: "Eggs, milk, butter, mushrooms, cheese, ham, spinach, tomatoes, salad, salt, pepper",
-                      price: 4.5,
+                      price: 8.5,
                       address: userA4. address,
                       photo: 'https://i.pinimg.com/originals/87/d1/57/87d157cdeb19dbdf7b6136bd9fa99273.jpg')
 mealA5 = Meal.create!(cook: userA5,
@@ -188,7 +188,7 @@ mealA5 = Meal.create!(cook: userA5,
                       description: "This is a classic of the Argentinian Cuisine. This high quality prime rib roast is whole grilled on the parilla. The meat comes directly from Argentinian producers and the chimichurri is, of course, homemade with only fresh ingredients.",
                       grams_per_dose: 1600,
                       ingredients: "Beef, various spicies, pepper, red wine vinegar, garlic cloves, shallot, fresh cilantro, fresh flat-leaf parsley, fresh oregano, extra-virgin olive oil",
-                      price: 22,
+                      price: 14,
                       address: userA5. address,
                       photo: 'https://www.viandesuisse.ch/filemount/_processed_/csm_3676892_ac83a123f7.jpg')
 
@@ -208,7 +208,7 @@ mealD2 = Meal.create!(cook: userD2,
                                     and tomato stew with rice, almost like a risotto, to become arroz de tamboril.",
                       grams_per_dose: 350,
                       ingredients: "Cilantro, onion,  cloves of garlic, cup parsley, pepper, tomato, paprika, green pepper, salt, pepper, extra virgin olive oil.",
-                      price: 3.20,
+                      price: 6.20,
                       address: userD2.address,
                       photo: 'https://fotos.web.sapo.io/i/o1b06043a/18261602_AjELi.jpeg')
 mealD3 = Meal.create!(cook: userD3,
